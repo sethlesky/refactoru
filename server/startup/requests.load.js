8ae0810727c5e6fc815a1e4a9289f9c6b45e6068
Seth Lesky <sethlesky@gmail.com>
2015-09-29 10:15:32 -0600

    clean up ui

--- server/startup/requests.load.js
+++ server/startup/requests.load.js
@@ -3,21 +3,21 @@ Meteor.startup(function() {
     var requests = [
       {
         'uid'     : 271898,
-        'status'  : 'open',
+        'status'  : 0,
         'time'    : new Date(),
         'name'    : 'Seth Lesky',
         'content' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus atque facere ratione velit molestias'
       },
       {
         'uid'     : 448783,
-        'status'  : 'closed',
+        'status'  : 1,
         'time'    : new Date(),
         'name'    : 'Bob Barker',
         'content' : 'A hack born out of idle curiosity by GitHub user #696: Caius Durling'
       },
       {
         'uid'     : 11048672,
-        'status'  : 'pending',
+        'status'  : 1,
         'time'    : new Date(),
         'name'    : 'Tucker Kline',
         'content' : 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.'
