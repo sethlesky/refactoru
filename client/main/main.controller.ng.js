44af0fe46660272eeb351eeb6e1a4c677669fdf9
Seth Lesky <sethlesky@gmail.com>
2015-10-01 21:01:54 -0600

    add admin frontend ui

--- client/main/main.controller.ng.js
+++ client/main/main.controller.ng.js
@@ -1,10 +1,25 @@
 'use strict'
 
 angular.module('refactorQApp')
-  .controller('MainCtrl', function($scope, $meteor, requestQueue) {
+  .controller('MainCtrl', function($scope, $meteor, requestQueue, $http) {
     $scope.requests = requestQueue.getQueue();
+    $scope.admins = [];
 
     $scope.addAdmin = function() {
+
+      $http.get('https://api.github.com/users/' + $scope.adminInput).then(function(response) {
+        console.log(response);
+        $scope.admins.push({
+          name: response.data.name,
+          login: response.data.login,
+          avatar: response.data.avatar_url,
+          uid: response.data.id
+        });
+
+      }, function(err) {
+        console.log(err);
+      });
+
       $scope.adminInput = '';
     }
 
